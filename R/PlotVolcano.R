1c4bc71c453cf82815c7d6b3e4373b4a12f0ae73
Zhang <zhangz@email.chop.edu>
2016-11-10 18:28:49 -0500

    volcano plot

--- R/PlotVolcano.R
+++ R/PlotVolcano.R
@@ -7,16 +7,22 @@ PlotVolcano <- function(fc, p, title='', plotly=FALSE) {
   y <- -1*log10(p);
   y[y==Inf] <- floor(max(y[y<Inf])) + 1.25; 
   z  <- sqrt(abs(fc*y));
-  cx <- 2 * z/max(z);
+  cx <- z/max(z);
   mx <- max(abs(fc), na.rm=TRUE);
   xlim <- c(-mx, mx);
   ylim <- c(0, max(y)+0.5); 
   
   if (plotly) {
     require(plotly);
-    sz  <- 6*(5-max(1, min(4, round(log10(length(fc))))));
+    sz  <- max(1, 5-max(1, min(4, round(log10(length(fc))))));
     mrk <- list(size = sz, symbol=2, line=list(width=.8, color='rgba(0, 0, 0, .3)'));
-    d   <- data.frame(x=fc, y=y, size=cx);
+    d   <- data.frame(X=fc, Y=y);
+    
+    p <- ggplot(data=d, aes(x=X, y=Y)) + 
+      theme(legend.position="none") + 
+      labs(x="Log2(fold change)",y="-Log10(p value)") +
+      geom_point(aes(text=names(fc)), size=0.8*sz, col='#DD0000DD');
+    ggplotly(p); 
     
     plot_ly(data=d, x=~fc, y=~y, type='scatter', mode='markers', text=rownames(d), hoverinfo="text", marker=mrk) %>%
       layout(
