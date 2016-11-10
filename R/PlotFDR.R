cea8b502bef594b61c83460fba9b2b284a519a25
Zhang <zhangz@email.chop.edu>
2016-11-10 00:36:25 -0500

    plotFDR

--- R/PlotFDR.R
+++ R/PlotFDR.R
@@ -1,4 +1,4 @@
-PlotFDR <- function(q, col='#3333FF', xlab='', ylab='', title='') {
+PlotFDR <- function(q, col='#3333FF', xlab='', ylab='', title='', plotly=FALSE) {
   
   q <- q[!is.na(q)];
   q <- round(q, 2); 
@@ -12,8 +12,19 @@ PlotFDR <- function(q, col='#3333FF', xlab='', ylab='', title='') {
   if (is.na(xlab) | xlab=='') xlab <- 'False discovery rate';
   if (is.na(ylab) | ylab=='') ylab <- 'Number of genes';
   
-  plot(1, type='n', log='y', xlab=xlab, ylab=ylab, cex.lab=2, xaxs='i', xlim=c(0, 1), ylim=c(1, length(q)), main=title);
-  abline(v=seq(0, 1, .05), lty=2, col=8);
-  lines(seq(min(q), 1, 0.01), n, lwd=2, col=col);
-  box();
+  if (plotly) {
+    require(plotly);
+    v <- seq(min(q), 1, 0.01);
+    plot_ly(x=v, y=n, type='scatter', mode='lines') %>%
+      layout(
+        showlegend=FALSE,
+        xaxis = list(title=xlab, zeroline=FALSE, showgrid=FALSE, showline=TRUE, showticklabels=TRUE),
+        yaxis = list(title=ylab, zeroline=FALSE, showgrid=TRUE, showline=TRUE, showticklabels=TRUE, type='log'));
+  } else {
+    plot(1, type='n', log='y', xlab=xlab, ylab=ylab, cex.lab=2, xaxs='i', xlim=c(0, 1), ylim=c(1, length(q)), main=title);
+    abline(v=seq(0, 1, .05), lty=2, col=8);
+    lines(seq(min(q), 1, 0.01), n, lwd=2, col=col);
+    box();    
+  }
+
 }
