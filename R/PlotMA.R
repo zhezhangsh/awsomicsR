0a5223c5c0310cb531e92e4b2ae8b70495ece48a
Zhang <zhangz@email.chop.edu>
2016-11-10 22:02:07 -0500

    volcano plot

--- R/PlotMA.R
+++ R/PlotMA.R
@@ -1,4 +1,4 @@
-PlotMA <- function(x, y, xlab='', ylab='', title='', plotly=FALSE) {
+PlotMA <- function(x, y, xlab='', ylab='', title='', plotly=FALSE, max.p=5000) {
   if (is.na(xlab) | xlab=='') xlab <- 'Log2(average expression)';
   if (is.na(ylab) | ylab=='') ylab <- 'Log2(fold change)';
   z <- max(abs(y), na.rm=TRUE);
@@ -8,16 +8,20 @@ PlotMA <- function(x, y, xlab='', ylab='', title='', plotly=FALSE) {
   
   if (plotly) {
     require(plotly); 
-    sz  <- 6*(5-max(1, min(4, round(log10(length(x))))));
-    mrk <- list(size = sz, symbol=0, line=list(width=.8, color='rgba(0, 0, 0, .3)'));
-    d   <- data.frame(x=x, y=y, lox=lox, loy=loy);
+    cut <- rev(sort(abs(y)))[min(max(100, max.p), length(y))]; 
+    sel <- abs(y) >= cut;
+    sz  <- abs(y/z)*10*(5-max(1, min(4, round(log10(length(x))))));
+    mrk <- list(size = sz[sel], symbol=0, line=list(width=.8, color='rgba(0, 0, 0, .3)'));
+    d   <- data.frame(x=x, y=y, txt=names(x))[sel, ];
     
-    plot_ly(data=d, x=~x, y=~y, type='scatter', mode='markers', text=names(x), hoverinfo="text", marker=mrk) %>%
-      add_trace(x=lox, y=loy, mode='lines') %>%
+    plot_ly(data=d, x=~x, y=~y, type='scatter', mode='markers', text=~txt, hoverinfo="text", marker=mrk) %>%
+      add_lines(x=lox, y=loy, text='')  %>%
       layout(
         showlegend=FALSE,
-        xaxis = list(title=xlab, zeroline=FALSE, showgrid=TRUE, showline=TRUE, showticklabels=TRUE),
-        yaxis = list(title=ylab, range=c(-z, z), zeroline=TRUE, showgrid=TRUE, showline=TRUE, showticklabels=TRUE));
+        xaxis = list(title=xlab, range=range(x), zeroline=FALSE, showgrid=TRUE, showline=TRUE, showticklabels=TRUE),
+        yaxis = list(title=ylab, range=c(-1.05*z, 1.05*z), zeroline=TRUE, showgrid=TRUE, showline=TRUE, showticklabels=TRUE),
+        shapes = list(list(type = "rect", xref = 'x', x0 = min(x), x1 = max(x), yref = 'y', y0 = -cut, y1 = cut, 
+                           fillcolor = 'rgb(38,78,152)', line = list(width=0), opacity = .5)));
   } else {
     if (title=='' | is.na(title)) par(mar=c(5,5,2,2)) else par(mar=c(5,5,3,2));
     plot(x, y, pch=18, col='#4444DD88', cex=.75, xlab=xlab, ylab=ylab, ylim=c(-z, z), main = title, cex.lab=2);
--- R/PlotVolcano.R
+++ R/PlotVolcano.R
@@ -1,4 +1,4 @@
-PlotVolcano <- function(fc, p, title='', plotly=FALSE) {
+PlotVolcano <- function(fc, p, title='', plotly=FALSE, max.p=5000) {
   
   i  <- which(!is.na(fc) & !is.na(p));
   fc <- fc[i];
@@ -6,7 +6,7 @@ PlotVolcano <- function(fc, p, title='', plotly=FALSE) {
   
   y <- -1*log10(p);
   y[y==Inf] <- floor(max(y[y<Inf])) + 1.25; 
-  z  <- sqrt(abs(fc*y));
+  z  <- sqrt(fc^2+y^2);
   cx <- z/max(z);
   mx <- max(abs(fc), na.rm=TRUE);
   xlim <- c(-mx, mx);
@@ -14,21 +14,25 @@ PlotVolcano <- function(fc, p, title='', plotly=FALSE) {
   
   if (plotly) {
     require(plotly);
-    sz  <- max(1, 5-max(1, min(4, round(log10(length(fc))))));
-    mrk <- list(size = sz, symbol=2, line=list(width=.8, color='rgba(0, 0, 0, .3)'));
-    d   <- data.frame(X=fc, Y=y);
+    cut <- rev(sort(z))[min(max(100, max.p), length(z))]
+    sel <- z >= cut;
+    sz  <- 5*max(1, 5-max(1, min(4, round(log10(length(fc))))));
+    mrk <- list(size = 2*cx[sel]*sz, symbol=2, color='rgba(196,0,0,.6)', line=list(width=.8, color='rgba(0, 0, 0, .3)'));
+    d   <- data.frame(fc=fc, y=y)[sel, ];
     
-    p <- ggplot(data=d, aes(x=X, y=Y)) + 
-      theme(legend.position="none") + 
-      labs(x="Log2(fold change)",y="-Log10(p value)") +
-      geom_point(aes(text=names(fc)), size=0.8*sz, col='#DD0000DD');
-    ggplotly(p); 
+    plot_ly(data=d, x=~fc, y=~y, type='scatter', mode='markers', text=rownames(d), hoverinfo="text", marker=mrk) %>%
+      layout(
+        showlegend=FALSE, 
+        xaxis = list(title='Log2(fold change)', range=xlim, zeroline=TRUE, showgrid=TRUE, showline=TRUE, showticklabels=TRUE),
+        yaxis = list(title='-Log10(p value)', range=ylim, zeroline=FALSE, showgrid=TRUE, showline=TRUE, showticklabels=TRUE),
+        shapes = list(list(type = 'circle', xref = 'x', x0 = -sqrt(cut), x1 = sqrt(cut), yref = 'y', y0 = -sqrt(cut), y1 = sqrt(cut), 
+                           fillcolor = 'rgba(196,0,0,.6)', line = list(width=0), opacity = 0.1)))
     
-#     plot_ly(data=d, x=~fc, y=~y, type='scatter', mode='markers', text=rownames(d), hoverinfo="text", marker=mrk) %>%
-#       layout(
-#         showlegend=FALSE,
-#         xaxis = list(title='Log2(fold change)', range=xlim, zeroline=TRUE, showgrid=TRUE, showline=TRUE, showticklabels=TRUE),
-#         yaxis = list(title='-Log10(p value)', range=ylim, zeroline=FALSE, showgrid=TRUE, showline=TRUE, showticklabels=TRUE));
+    #     p <- ggplot(data=d, aes(x=X, y=Y)) + 
+    #       theme(legend.position="none") + 
+    #       labs(x="Log2(fold change)",y="-Log10(p value)") +
+    #       geom_point(aes(text=names(fc)), size=0.8*sz, col='#DD0000DD');
+    #     ggplotly(p); 
   } else {
     if (title=='' | is.na(title)) par(mar=c(5,5,2,2)) else par(mar=c(5,5,3,2));
     
