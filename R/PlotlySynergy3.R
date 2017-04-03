e53e332503278ec782195857c6084dc292114f26
Zhang <zhangz@email.chop.edu>
2017-04-03 16:59:34 -0400

    synergy

--- R/PlotlySynergy3.R
+++ R/PlotlySynergy3.R
@@ -1,3 +1,48 @@
+PlotSynergy3 <- function(v, name=c('', '', ''), shape=c('circle', 'rect'), size=5, title='', legend='', panel.size=600,
+                         color.min=min(v), color.max=max(v), color.type=GetColorPanelTypes()[1]) {
+  require(awsomics);
+  
+  if (name[1]=='') name[1] <- 'A';
+  if (name[2]=='') name[2] <- 'B';
+  if (name[3]=='') name[3] <- 'C';
+  
+  size <- max(1, min(6, size)); 
+  
+  if (color.type[1] %in% GetColorPanelTypes()) color.type <- color.type[1] else color.type <- 'blue-pink'; 
+  color <- GetColors(128, color.type);
+  if (color.type == 'heat') color <- rev(color);
+  
+  x <- c(0, -sqrt(0.75), sqrt(0.75), -sqrt(0.27), sqrt(0.27), 0, 0);
+  y <- c(1, -0.5, -0.5, 0.3, 0.3, -0.6, 0); 
+  w <- max(0.5, min(1.5, size));
+  
+  rng <- sort(c(color.min, color.max));
+  rng <- FormatNumeric(matrix(rng, nc=1))[, 1];
+  col <- round(length(rgb)*(v-rng[1])/(rng[2]-rng[1]))
+  col <- color[col];
+  
+  plot(0, 0, type = "n", xlim = c(-1.25, 1.25), ylim = c(-1.25, 1.25), axes = FALSE, bty = "n", xaxs = "i", yaxs = "i", xlab = "", ylab = ""); 
+  lines(x=x[c(1,4,2)], y=y[c(1,4,2)], col="#33333333", lwd=w);
+  lines(x=x[c(2,6,3)], y=y[c(2,6,3)], col="#33333333", lwd=w);
+  lines(x=x[c(3,5,1)], y=y[c(3,5,1)], col="#33333333", lwd=w);
+  lines(x=x[c(1, 7)],  y=y[c(1, 7)],  col="#33333333", lwd=w);
+  lines(x=x[c(2, 7)],  y=y[c(2, 7)],  col="#33333333", lwd=w);
+  lines(x=x[c(3, 7)],  y=y[c(3, 7)],  col="#33333333", lwd=w);
+  for (i in 1:length(x)) symbols(x=x[i], y=y[i], circles = size/33, inches = FALSE, add = TRUE, bg = col[i]);
+  
+  xt <- x[1:3];
+  yt <- c(y[1]+size/33+0.06, y[2]-size/33-0.075, y[3]-size/33-0.075);
+  text(xt, yt, labels = name, cex=1.5);
+  
+  xl <- seq(x[2]-size/33, x[3]+size/33, length.out = 1+length(color))
+  lapply(1:(length(xl)-1), function(i) 
+    rect(xleft = xl[i], xright = xl[i+1], ybottom = -1.1, ytop = -1, lwd=0, col = color[i])) -> z;
+  text(x=min(xl), y=-0.95, labels=rng[1]);
+  text(x=max(xl), y=-0.95, labels=rng[2]);
+  
+};
+
+
 PlotlySynergy3 <- function(v, name=c('', '', ''), shape=c('circle', 'rect'), size=5, title='', legend='', panel.size=600,
                            color.min=min(v), color.max=max(v), color.type=GetColorPanelTypes()[1]) {
   
