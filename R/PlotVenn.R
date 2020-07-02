b66b9da241782d955cfa82a02ecaf72a54ba862d
Zhe Zhang <zhezhangsh@users.noreply.github.com>
2020-07-02 09:51:26 -0400

    Update PlotVenn.R

--- R/PlotVenn.R
+++ R/PlotVenn.R
@@ -1,58 +1,10 @@
-# Draw a 2-set Venn diagram
-PlotlyVenn <- function(s1, s2, names=c('Set1', 'Set2'),  universe=c(), fisher=TRUE, title='') {
-  require(plotly);
-
-  if (length(universe)>=2) {
-    s1<-intersect(s1, universe);
-    s2<-intersect(s2, universe);
-  };
-  if (length(s1)==0&length(s2)==0) {
-    plotly_empty()
-  } else {
-    s0 <- intersect(s1, s2);
-    l0 <- length(s0);
-    l1 <- length(s1)-l0;
-    l2 <- length(s2)-l0;
-    l  <- length(universe)-l0-l1-l2;
-
-    x  <- c(1, 2, 3, 2, 1, 3);
-    y  <- c(2, 2, 2, 0.75, 3.5, 3.5);
-    tt <- c(l1, l0, l2, l, names);
-
-    # Run fisher
-    if (fisher) {
-      f  <- fisher.test(matrix(c(l, l1, l2, l0), nr=2));
-      p  <- f[[1]];
-      ci <- f[[2]];
-      or <- f[[3]];
-
-      if (p<0.0001) p <- format(p, scientific=TRUE, digit=2) else p <- round(p, 4);
-      p  <- gsub(' ', '', p);
-      ln <- paste(
-        paste('Odds ratio=', round(or, 2), sep=''), '; ',
-        paste('95% C.I.=(', round(ci[1], 2), ', ', round(ci[2], 2), ')', sep=''), '; ',
-        paste('p=', p), sep='');
-      x  <- c(x, 2);
-      y  <- c(y, 0.25);
-      tt <- c(tt, ln);
-    } # end of fisher test
-
-    xaxis <- list(title='', range=c(0, 4), zeroline=FALSE, showgrid=FALSE, showline=FALSE, autotick=FALSE, showticklabels=FALSE);
-    yaxis <- list(title='', range=c(0, 4), zeroline=FALSE, showgrid=FALSE, showline=FALSE, autotick=FALSE, showticklabels=FALSE);
-    tfont <- list(family = "sans serif", size = 16, color = toRGB("grey50"));
-
-    shap1 <- list(type='circle', xref='x', yref='y', x0=0.5, x1=2.5, y0=1, y1=3, fillcolor='blue', opacity=0.3, line=list(color='black', width=1));
-    shap2 <- list(type='circle', xref='x', yref='y', x0=1.5, x1=3.5, y0=1, y1=3, fillcolor='red',  opacity=0.3, line=list(color='black', width=1));
-    shap3 <- list(type='rect', xref='x', yref='y', x0=0, x1=4, y0=0, y1=4, fillcolor='none',  opacity=1);
-
-    plot_ly(x=x, y=y, type='scatter', mode='text', text=tt, textfont = tfont) %>%
-      layout(shapes=list(shap1, shap2, shap3), title=title, showlegend=FALSE, margin=list(b=1,l=1,r=1,t=1), xaxis=xaxis, yaxis=yaxis)
-  }
-};
-
-##########################################################
 PlotVenn<-function(s1, s2, names=c('Set1', 'Set2'),  universe=c(), fisher=TRUE, pdf=FALSE,
                    plot.new=FALSE, lwd=3, cex=1, title='', plotly=FALSE) {
+  # s1        Genes in gene set 1
+  # s2        Genes in gene set 2
+  # names     Names of 2 gene set
+  # universe  Background genes
+  
   out<-list();
 
   if (length(universe)>=2) {
@@ -156,6 +108,59 @@ PlotVenn<-function(s1, s2, names=c('Set1', 'Set2'),  universe=c(), fisher=TRUE, 
   out;
 }
 
+# Draw a 2-set Venn diagram
+PlotlyVenn <- function(s1, s2, names=c('Set1', 'Set2'),  universe=c(), fisher=TRUE, title='') {
+  require(plotly);
+
+  if (length(universe)>=2) {
+    s1<-intersect(s1, universe);
+    s2<-intersect(s2, universe);
+  };
+  if (length(s1)==0&length(s2)==0) {
+    plotly_empty()
+  } else {
+    s0 <- intersect(s1, s2);
+    l0 <- length(s0);
+    l1 <- length(s1)-l0;
+    l2 <- length(s2)-l0;
+    l  <- length(universe)-l0-l1-l2;
+
+    x  <- c(1, 2, 3, 2, 1, 3);
+    y  <- c(2, 2, 2, 0.75, 3.5, 3.5);
+    tt <- c(l1, l0, l2, l, names);
+
+    # Run fisher
+    if (fisher) {
+      f  <- fisher.test(matrix(c(l, l1, l2, l0), nr=2));
+      p  <- f[[1]];
+      ci <- f[[2]];
+      or <- f[[3]];
+
+      if (p<0.0001) p <- format(p, scientific=TRUE, digit=2) else p <- round(p, 4);
+      p  <- gsub(' ', '', p);
+      ln <- paste(
+        paste('Odds ratio=', round(or, 2), sep=''), '; ',
+        paste('95% C.I.=(', round(ci[1], 2), ', ', round(ci[2], 2), ')', sep=''), '; ',
+        paste('p=', p), sep='');
+      x  <- c(x, 2);
+      y  <- c(y, 0.25);
+      tt <- c(tt, ln);
+    } # end of fisher test
+
+    xaxis <- list(title='', range=c(0, 4), zeroline=FALSE, showgrid=FALSE, showline=FALSE, autotick=FALSE, showticklabels=FALSE);
+    yaxis <- list(title='', range=c(0, 4), zeroline=FALSE, showgrid=FALSE, showline=FALSE, autotick=FALSE, showticklabels=FALSE);
+    tfont <- list(family = "sans serif", size = 16, color = toRGB("grey50"));
+
+    shap1 <- list(type='circle', xref='x', yref='y', x0=0.5, x1=2.5, y0=1, y1=3, fillcolor='blue', opacity=0.3, line=list(color='black', width=1));
+    shap2 <- list(type='circle', xref='x', yref='y', x0=1.5, x1=3.5, y0=1, y1=3, fillcolor='red',  opacity=0.3, line=list(color='black', width=1));
+    shap3 <- list(type='rect', xref='x', yref='y', x0=0, x1=4, y0=0, y1=4, fillcolor='none',  opacity=1);
+
+    plot_ly(x=x, y=y, type='scatter', mode='text', text=tt, textfont = tfont) %>%
+      layout(shapes=list(shap1, shap2, shap3), title=title, showlegend=FALSE, margin=list(b=1,l=1,r=1,t=1), xaxis=xaxis, yaxis=yaxis)
+  }
+};
+
+##########################################################
 PlotVenn3Way<-function(s1, s2=NA, s3=NA, names=rep('', 3), fill=c('#E74C3C', '#3498DB', '#2ECC71'), plot.new=TRUE, ...) {
 
   if(!identical(s2, NA) & !identical(s3, NA)) sets<-list(s1, s2, s3) else sets<-s1;
